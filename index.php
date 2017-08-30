6f3dc10fff49a64f0bd394c04fe9bf6c6bca9abc
crack81 <jgarnica@ant.com.mx>
2017-08-30 13:39:46 -0500

    changed the text on html to english

--- index.php
+++ index.php
@@ -1,27 +1,42 @@
-    <!DOCTYPE html>
-    <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1.0">
-        <meta http-equiv="X-UA-Compatible" content="ie=edge">
-        <title>Document</title>
-    </head>
-    <body>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Dynamic text on image</title>
+</head>
+<body>
 
-       <div class="setting-image">
-            <label for="imagetext">Titulo de la imagen</label> <br/>
-            <input type="text"  id="imagetext" name="imagetext"/>
-            <input type="button" value="asginar mensaje"/>
-       </div>
+    <div class="setting-image">
+        <label for="imagetext">Text for assign</label> <br/>
+        <input type="text"  id="imagetext" name="imagetext"/>
+        <input type="button" value="assign text into image" onclick="generateText()" />
+    </div>
 
 
-       <br/>
-       <div class="image" id="default-image">
-            <img src="createImage.php" alt="tigerimage"  onclik="generateText()">
-       </div>
-       <div class="image" id="custome-image"> </div>
+    <br/>
+    <div class="image">
+        <img  id="default-image" src="createImage.php" alt="noimage"  onclik="generateText()">
+    </div>
 
-       <script src="script.js">
-       </script>
-    </body>
-    </html>
+    <!--Cargamos el javascript-->
+    <script>
+        function generateText(){
+        var currentText = document.getElementById("imagetext").value;
+        var textLen = currentText.length;
+        var imgchanged = document.getElementById("default-image");
+
+        if(textLen>0){
+
+            var fontsize = 40;
+            if(textLen>10) fontsize = 30;
+            if(textLen>20) fontsize = 20;
+
+            imgchanged.src = 'createImage.php?fontsize='+fontsize+'&text='+currentText;
+        }
+        else imgchanged.src = 'createImage.php?';
+    }
+    </script>
+</body>
+</html>
